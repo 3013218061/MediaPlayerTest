0314ec15d182b93f1861224beb60ef8db202fe54
xiaoyuer <605451377@qq.com>
2017-02-22 20:49:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xiaoyuer/testvedio/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.xiaoyuer.testvedio;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.song);
+        //开始
+        Button playButton = (Button) findViewById(R.id.play_button);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "play", Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();
+            }
+        });
+        //暂停
+        Button pauseButton = (Button) findViewById(R.id.pause_button);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "pause", Toast.LENGTH_SHORT).show();
+                mediaPlayer.pause();
+            }
+        });
+    }
+}
